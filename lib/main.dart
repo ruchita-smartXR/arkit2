beb1c68826f5fce40c584f3d9fe38837a8072665
Karan Shirur <k9shirur@gmail.com>
2023-11-06 15:45:22 +0530

    ios test code

---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:arkit_plugin/arkit_plugin.dart';
+
+import 'package:vector_math/vector_math_64.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        //colorScheme: ColorScheme.fromSeed(seedColor: Colors.),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late ARKitController arkitController;
+  Timer? timer;
+  @override
+  void dispose() {
+    timer?.cancel();
+    arkitController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(title: const Text('Earth Sample')),
+    body: Container(
+      child: ARKitSceneView(
+        onARKitViewCreated: onARKitViewCreated,
+      ),
+    ),
+  );
+
+  void onARKitViewCreated(ARKitController arkitController) {
+    this.arkitController = arkitController;
+
+    final material = ARKitMaterial(
+      lightingModelName: ARKitLightingModel.lambert,
+      diffuse: ARKitMaterialProperty.image('earthdemo.jpg'),
+    );
+    final sphere = ARKitSphere(
+      materials: [material],
+      radius: 0.1,
+    );
+
+    final node = ARKitNode(
+      geometry: sphere,
+      position: Vector3(0, 0, -0.5),
+      eulerAngles: Vector3.zero(),
+    );
+    this.arkitController.add(node);
+
+    timer = Timer.periodic(const Duration(milliseconds: 50), (timer) {
+      final rotation = node.eulerAngles;
+      rotation.x += 0.01;
+      node.eulerAngles = rotation;
+    });
+  }
+
+}
